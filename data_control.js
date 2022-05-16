c7e46eaa8535b2134ed597571c49ae641c066d58
JcbBrtn <jcbbrtn2@gmail.com>
2022-05-16 16:26:16 -0400

    Created Radio button input area

--- data_control.js
+++ data_control.js
@@ -12,8 +12,9 @@ function Update_Player2_Label(){
     player2_label.innerText=player2.value;
 }
 
-function Focus_On_Submit_Button(){
-    document.getElementById("Submit_Btn").focus();
+function Toggle_Table(){
+    table = document.getElementById("match_data");
+    table.classList.toggle("invisible");
 }
 
 function Add_Point_Data() {
@@ -34,11 +35,58 @@ function Add_Point_Data() {
     //insert the data for each new column
     Point_Number.innerHTML = point_count;
     Shot_Count.innerHTML = document.getElementById("Shot_Count").value;
-    Side.innerHTML = document.getElementById("Side").value;
-    Shot_Type.innerHTML = document.getElementById("Shot").value;
-    Miss_Place.innerHTML = document.getElementById("Place").value;
-    End_Type.innerHTML = document.getElementById("Type").value;
 
+    //Check the side radios for forehand or backhand
+    if (document.getElementById("Forehand_Radio").checked){
+        Side.innerHTML = document.getElementById("Forehand_Label").innerText; 
+    }
+    else if (document.getElementById("Backhand_Radio").checked){
+        Side.innerHTML = document.getElementById("Backhand_Label").innerText; 
+    }
+
+    //Check the Type radios for the type of shot missed
+    if (document.getElementById("Serve_Radio").checked){
+        Shot_Type.innerHTML = document.getElementById("Serve_Label").innerText;
+    }
+    if (document.getElementById("Drive_Radio").checked){
+        Shot_Type.innerHTML = document.getElementById("Drive_Label").innerText;
+    }
+    if (document.getElementById("Volley_Radio").checked){
+        Shot_Type.innerHTML = document.getElementById("Volley_Label").innerText;
+    }
+    if (document.getElementById("Lob_Radio").checked){
+        Shot_Type.innerHTML = document.getElementById("Lob_Label").innerText;
+    }
+    if (document.getElementById("Overhead_Radio").checked){
+        Shot_Type.innerHTML = document.getElementById("Overhead_Label").innerText;
+    }
+
+    //Check to see how the shot was missed
+    if (document.getElementById("None_Radio").checked){
+        Miss_Place.innerHTML = "";
+    }
+    if (document.getElementById("Long_Radio").checked){
+        Miss_Place.innerHTML = document.getElementById("Long_Label").innerText;
+    }
+    if (document.getElementById("Wide_Radio").checked){
+        Miss_Place.innerHTML = document.getElementById("Wide_Label").innerText;
+    }
+    if (document.getElementById("Net_Radio").checked){
+        Miss_Place.innerHTML = document.getElementById("Net_Label").innerText;
+    }
+
+    //Check to see what kind of error it was
+    if (document.getElementById("Winner_Radio").checked){
+        End_Type.innerHTML = document.getElementById("Winner_Label").innerText;
+    }
+    if (document.getElementById("UE_Radio").checked){
+        End_Type.innerHTML = document.getElementById("UE_Label").innerText;
+    }
+    if (document.getElementById("FE_Radio").checked){
+        End_Type.innerHTML = document.getElementById("FE_Label").innerText;
+    }
+
+    //Get data from Player Ended radio buttons
     if (document.getElementById("Player1_Radio").checked){
         Ended_By.innerHTML = document.getElementById("Player1_Label").innerText; 
     }
@@ -49,7 +97,4 @@ function Add_Point_Data() {
 
     //increment the point count
     point_count++;
-
-    //Change focus back to Shot count for ease of keyboard use
-    document.getElementById("Shot_Count").focus()
 }
